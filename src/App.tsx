f3455d134073b3ecebeffedd7ff63c6ebddb7dbf
Rajkumar Rathod <rajkumar.rathod@thinkitive.com>
2023-10-14 21:45:32 +0530

    main branch added

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Grid } from '@mui/material'
+import './App.css'
+import Navbar from './Navbar'
+function App() {
+  return (
+    <>
+    <Navbar/>
+      <Grid>
+        
+      </Grid>
+    </>
+  )
+}
+
+export default App
